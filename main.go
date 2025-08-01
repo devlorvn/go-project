0f7aea3d95208f12435389113aceebd2d6d25368
luannt-algolabs <luannt@algolabs.vn>
2025-08-01 15:43:14 +0700

    feat: httpHandle, serve multiplexer and server mark

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/devlorvn/go-project/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Ping Pong")
-		d, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Ooops", http.StatusBadRequest)
-			return
-		}
-		log.Printf("Data %s\n", d)
-		fmt.Fprintf(w, "Hello %s", d)
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	http.HandleFunc("/hello", hh.ServeHTTP)
 
 	http.ListenAndServe(":9090", nil)
 }
