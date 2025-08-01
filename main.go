7442d70b17f473f514e395465ec4ff5a3d57c9d4
luannt-algolabs <luannt@algolabs.vn>
2025-08-01 14:53:01 +0700

    feat: first request with req, response, and http error handle

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -8,6 +10,14 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Ping Pong")
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Ooops", http.StatusBadRequest)
+			return
+		}
+		log.Printf("Data %s\n", d)
+		fmt.Fprintf(w, "Hello %s", d)
 	})
+
 	http.ListenAndServe(":9090", nil)
 }
